33a0dd81fb3475749e6689ce193b583f59a69ffa
Gary Chen <cpldhycxc@gmail.com>
2020-09-02 23:31:31 +1000

    add

--- app/data/resume/review.js
+++ app/data/resume/review.js
@@ -4,6 +4,11 @@ const review = [
     authors: 'Xiaocong Chen, Lina Yao, and Yu Zhang',
     link: 'https://arxiv.org/abs/2004.05645',
   },
+  {
+    title: 'Momentum Contrastive Learning for Few-Shot COVID-19 Diagnosis from Chest CT Images',
+    authors: 'Xiaocong Chen, Lina Yao, Tao Zhou, Jinming Dong and Yu Zhang',
+    link: 'https://arxiv.org/pdf/2006.13276',
+  },
 ];
 
 export default review;
